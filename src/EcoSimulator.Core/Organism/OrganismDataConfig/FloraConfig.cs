e20ef68459cfab73862722d62c29f72de3ce39f0
Snt <sant85082@gmail.com>
2026-01-24 23:35:23 -0500

    Feat: Implements FloraConfig class has the values that the FloraOrganism need

---
+++ src/EcoSimulator.Core/Organism/OrganismDataConfig/FloraConfig.cs
@@ -0,0 +1,8 @@
+namespace EcoSimulator.Core.Organism.OrganismDataConfig;
+
+public class FloraConfig
+{
+    public double Energy {get; set;}
+    public double EnergyGiven{get; set;}
+    public double HungryMinus{get; set;}
+}
