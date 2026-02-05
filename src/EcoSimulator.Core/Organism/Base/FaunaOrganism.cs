dd9a1c49ed63c6f7cb8b6960fb65ef75ade79d5e
Snt <sant85082@gmail.com>
2026-02-04 19:49:19 -0500

    Refactor: Start logic migration to base class Fauna Organism

--- src/EcoSimulator.Core/Organism/Base/FaunaOrganism.cs
+++ src/EcoSimulator.Core/Organism/Base/FaunaOrganism.cs
@@ -1,19 +1,43 @@
 namespace EcoSimulator.Core.Organism.Base;
 
 using System;
+using EcoSimulator.Core.Interface;
 using EcoSimulator.Core.Interface.IOrganism;
+using EcoSimulator.Core.Organism.OrganismDataConfig;
 
-public abstract class FaunaOrganism : Organism
+public abstract class FaunaOrganism : Organism, IGrow, ICheckMetabolism, IEat, IReproduce, IDie
 {
+    protected readonly FaunaConfig _config;
     public bool HasEaten {get; protected set;}
     public double Hunger {get; protected set;}
     public double ReproduceEnergy {get; protected set;}
 
-    public FaunaOrganism(double objEnergy, double objReproduceEnergy) : base(objEnergy)
+    public FaunaOrganism(FaunaConfig objConfig, double objEnergy, double objReproduceEnergy) : base(objEnergy)
     {
+        _config = objConfig;
         HasEaten = false;
         Hunger = 0.0;
         ReproduceEnergy = objReproduceEnergy;
+    }
+
+    protected virtual void Grow()
+    {
+        if(IsDead) return;
+
+        Age++;
+        CheckMetabolism();
+    }
+
+    protected virtual void CheckMetabolism()
+    {
+        double energyCost = (Age < _config.AdultAge) ? _config.EnergySpentYounger : _config.EnergySpentOlder;
+        ApplyMetabolism(energyCost, energyCost);
+    }
+
+    public virtual void Eat(Organism food)
+    {
+        // Base logic: Dead animals cannot eat
+        if (IsDead) return;
     }
 
     protected void ApplyMetabolism(double energySpent, double hungryGain)
