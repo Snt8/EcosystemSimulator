cb5a9f84aceb69f5966455d6f70def194d4537bd
Snt <sant85082@gmail.com>
2026-02-04 19:28:12 -0500

    Refactor: Apply the Fauna constants for Fauna Config

---
+++ src/EcoSimulator.Core/Organism/OrganismDataConfig/FaunaConfig.cs
@@ -0,0 +1,13 @@
+namespace EcoSimulator.Core.Organism.OrganismDataConfig;
+
+public class FaunaConfig
+{
+    //Standart Fauna Organism constant atributes
+    public double EnergySpentYounger {get; set;}
+    public double EnergySpentOlder {get; set;}
+    public int AdultAge {get; set;}
+    public int MaxOlderAge {get; set;}
+    public double MaxEnergy {get; set;}
+    public double MinimumEnergy {get; set;}
+    public double FaunaReproduceEnergy {get; set;}
+}
