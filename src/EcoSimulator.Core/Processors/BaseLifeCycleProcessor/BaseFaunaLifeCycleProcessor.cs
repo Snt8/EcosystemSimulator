153796aa35097b51b1f758e6d41494029d447d02
Snt <sant85082@gmail.com>
2026-02-21 00:00:00 -0500

    feat: implements initial structure of the base fauna processor class

---
+++ src/EcoSimulator.Core/Processors/BaseLifeCycleProcessor/BaseFaunaLifeCycleProcessor.cs
@@ -0,0 +1,30 @@
+﻿using EcoSimulator.Core.Interface.ILifeCycleProcessor;
+using EcoSimulator.Core.Organism.Base;
+
+namespace EcoSimulator.Core.Processors.BaseLifeCycleProcessor;
+
+    public class FaunaLifeCycleProcessor : ILifeFaunaProcessor
+{
+
+    public void CallGrow()
+    {
+
+    }
+
+    public IEnumerable<Organism> CallEat()
+    {
+
+    }
+
+    public IEnumerable<Organism> CallDie()
+    {
+
+    }
+
+    public IEnumerable<Organism> CallReproduce()
+    {
+
+    }
+
+
+}
