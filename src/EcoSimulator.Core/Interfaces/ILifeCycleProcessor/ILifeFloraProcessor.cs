762c3ea5c7b2192445976f91ce0eeffb0f332d64
Snt <sant85082@gmail.com>
2026-02-20 23:59:20 -0500

    refactor: comment out logic in Interfaces directory

--- src/EcoSimulator.Core/Interfaces/ILifeCycleProcessor/ILifeFloraProcessor.cs
+++ src/EcoSimulator.Core/Interfaces/ILifeCycleProcessor/ILifeFloraProcessor.cs
@@ -1,8 +1,13 @@
 namespace EcoSimulator.Core.Interface.ILifeCycleProcessor;
+
+using System.Xml.Serialization;
 using EcoSimulator.Core.Organism.Base;
 
 public interface ILifeFloraProcessor
 {
+    //First turn the processor calls Grow method in a FloraOrganism subclass
     void CallGrow();
+
+    //Second turn the processor calls Die method if a FloraOrganism subclass dies
     IEnumerable<Organism> CallDie();
 }
