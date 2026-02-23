8cd532628ac10f4125f89e92e228fbb7bfb49a99
Snt <sant85082@gmail.com>
2026-02-22 19:34:55 -0500

    feat: add the processor atributes

--- src/EcoSimulator.Core/Processors/BaseLifeCycleProcessor/BaseFaunaLifeCycleProcessor.cs
+++ src/EcoSimulator.Core/Processors/BaseLifeCycleProcessor/BaseFaunaLifeCycleProcessor.cs
@@ -1,10 +1,11 @@
 ﻿using EcoSimulator.Core.Interface.ILifeCycleProcessor;
 using EcoSimulator.Core.Organism.Base;
-
 namespace EcoSimulator.Core.Processors.BaseLifeCycleProcessor;
 
     public class FaunaLifeCycleProcessor : ILifeFaunaProcessor
 {
+    public List<Organism> MasterOrganism {get; private set;}
+    public List<Organism> MasterFoodOrganism {get; private set;}
 
     public void CallGrow()
     {
